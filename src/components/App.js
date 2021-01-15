092ac6a9ba11d7954fc09eb6a2622cb6e1fdd211
Your Name <stasgill@gmail.com>
2021-01-15 14:47:17 +0200

    home work 2

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react'
+import ContactList from './contactList/ContactList'
+import { v4 as uuidv4 } from 'uuid';
+
+export default class App extends Component {
+    state = {
+        contacts: [
+            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
+        name: '',
+        number: '',
+        filter: '',
+      }
+    onChange = (e) => {
+          this.setState({ [e.target.name]: e.target.value });
+       }
+    onSubmit = e => {
+           e.preventDefault();
+           this.setState(prevState => ({
+            contacts: [...prevState.contacts, {name: this.state.name,id:uuidv4(),number: this.state.number }],name: '',number:''
+        }));
+       }
+    handleDelete =  e => {
+        const id = e.target.name
+        this.setState(prevState => ({
+            contacts: [...prevState.contacts.filter(item => item.id !== id), ]
+        }));
+       
+       }
+    handleFilter = (e) => {
+            console.log(e.target.value)
+            this.setState({ filter: e.target.value });
+       }
+    filter = () => {
+        const filtered = this.state.contacts.filter(item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+        return filtered
+       }
+
+    render() {
+        return(  <>
+        <div className="addPanel">
+            <h1>Phonebook</h1>
+            <form onSubmit={this.onSubmit}>
+                <h2>Name</h2>
+                <input type="text" className="addPanelInput" placeholder="Type name..." name="name" value={this.state.name} onChange={this.onChange}></input>
+                <h2>Number</h2>
+                <input type="text" className="addPanelInput" placeholder="Type number..." name="number" value={this.state.number} onChange={this.onChange}></input>
+                <br/><button type="submit" className="handleButton">Add contact</button>
+            </form>
+        </div>
+        <div className="addPanel">
+        <h2>Contacts</h2>
+           <input name="filter" onChange={this.onChange} className='addPanelInput' placeholder='Find contact...'></input>
+            <ul className="list">
+                {
+                    this.filter().map( contact => <ContactList contact={contact} handleDelete={this.handleDelete} key={contact.id}/>) 
+                }
+            </ul>
+                </div>
+        </>)
+    }
+}
+
